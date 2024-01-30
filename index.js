7d0abdc9656cd6a47da06f49015b2684197f315a
Jorge Vega <83831472+HX-JVega@users.noreply.github.com>
2024-01-30 09:16:20 -0300

    fix cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors())
 
 app.get("/movies", (req, res) => {
   const movies = [
